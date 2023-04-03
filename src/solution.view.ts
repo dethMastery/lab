e828ef38f4c075d29d58682961970179a5b399ff
dethMastery <me@suphakit.net>
2023-04-03 16:24:15 +0700

    chore: update view

--- src/solution.view.ts
+++ src/solution.view.ts
@@ -2,26 +2,12 @@ export default class SolutionView {
   listingNumber(array: number[]) {
     let i: number = 0
 
-    for (let no of array) {
-      switch (i) {
-        case 0:
-          console.log('1st smallest = ', no);
-          break;
-
-        case 1:
-          console.log('2nd smallest = ', no);
-          break;
-
-        case 2:
-          console.log('3rd smallest = ', no);
-          break;
-      
-        default:
-          console.log(`${i + 1}th smallest = `, no);
-          break;
+    for (i; i<2; i++) {
+      if (i == 0) {
+        console.log('1st smallest = ', array[i]);
+      } else {
+        console.log('2nd smallest = ', array[i]);
       }
-
-      i++
     }
   }
 }
